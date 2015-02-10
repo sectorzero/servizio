69baeb18bedc1fdc7dafb8c7d59f403e40018b34
sectorzero <sndshd@gmail.com>
2015-02-10 11:12:52 -0800

    Added placeholder for base provided guice bindings : BaseConfigurationModule

---
+++ src/main/java/org/sectorzero/server/framework/dropwizard/app/configuration/guice/BaseConfigurationModule.java
@@ -0,0 +1,11 @@
+package org.sectorzero.server.framework.dropwizard.app.configuration.guice;
+
+import com.google.inject.AbstractModule;
+
+public class BaseConfigurationModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+    }
+
+}
